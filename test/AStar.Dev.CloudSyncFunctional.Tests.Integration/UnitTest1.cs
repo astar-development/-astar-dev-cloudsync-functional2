fd8262e1049cc1a8dc5c2a3e42b2569af66fdd5f
Jason Barden <jason.barden1@outlook.com>
2026-05-29 14:15:35 +0100

    Add the basic projects

---
+++ test/AStar.Dev.CloudSyncFunctional.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AStar.Dev.CloudSyncFunctional.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ test/AStar.Dev.FunctionalParadigm.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AStar.Dev.FunctionalParadigm.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
